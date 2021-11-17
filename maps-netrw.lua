d6f51af208be7871550cf5df6d05c51308f880cb
nd28 <sutharnilesh1901@gmail.com>
2021-11-17 20:47:15 +0530

    initial commit

---
+++ maps-netrw.lua
@@ -0,0 +1,4 @@
+nnoremap <leader>nc :Lexplore<CR>
+    " `mnc` : Will open Netrw in the current working directory.
+nnoremap <leader>nf :Lexplore %:p:h<CR>
+    " `mnf` : Will open Netrw in the directory of the current filennoremap <leader>nf :Lexplore %:p:h<CR>
